c4ef1d019ec9a3ac8b5ff04bbae80e7be758b4b6
Saurabh Dewangan <sdewangan021@gmail.com>
2022-07-31 16:38:08 +0530

    added api call and card UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
-console.log(
-    " this is a test string"
-)
+function setUI(data) {
+    document.getElementById('n1').innerHTML = data[0].name
+    document.getElementById('e1').innerHTML = data[0].email
+
+    document.getElementById('n2').innerHTML = data[1].name
+    document.getElementById('e2').innerHTML = data[1].email
+}
+
+function fetchUserData() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => setUI(data))
+}
+
+fetchUserData()
