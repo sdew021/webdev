78cac7113719600ca2df989ab881d7932d4133d6
Saurabh Dewangan <sdewangan021@gmail.com>
2022-08-03 11:41:30 +0530

    dynamic logic to create cards and set data from the api

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,14 @@
 function setUI(data) {
-    document.getElementById('n1').innerHTML = data[0].name
-    document.getElementById('e1').innerHTML = data[0].email
-
-    document.getElementById('n2').innerHTML = data[1].name
-    document.getElementById('e2').innerHTML = data[1].email
+    for (let i = 0; i < 10; i++) {
+        document.getElementById('n' + i).innerHTML = data[i].name
+        document.getElementById('e' + i).innerHTML = data[i].email
+    }
 }
+
 
 function fetchUserData() {
     fetch("https://jsonplaceholder.typicode.com/users")
         .then(res => res.json())
         .then(data => setUI(data))
 }
-
 fetchUserData()
